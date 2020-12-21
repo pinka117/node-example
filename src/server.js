33502f43a35ebec606355217e1e9864c7f90181e
linda paolieri <me@lindapaolieri.com>
2020-12-21 12:13:19 +0100

    inizialize

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import app from './app.js';
+
+// Get port from environment and store in Express.
+const port = process.env.PORT || '3000';
+
+// Get port from environment and store in Express.
+app.listen(port, () => console.log(`App listening on port ${port}`));
