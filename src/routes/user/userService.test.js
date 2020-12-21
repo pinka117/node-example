4a191355bf076ebf9d3177c582a6cf6668ce11c1
linda paolieri <me@lindapaolieri.com>
2020-12-21 18:47:27 +0100

    fix eslint errors

--- src/routes/user/userService.test.js
+++ src/routes/user/userService.test.js
@@ -1,38 +1,30 @@
-import Mockingoose from 'mockingoose';
+import Mockingoose from "mockingoose";
 import { createUser } from "./userService.js";
 
-const mockingoose=Mockingoose.default;
+const mockingoose = Mockingoose.default;
 
-test('Create duplicate user', async () => {
-    const _doc = {
-        _id: '507f191e810c19729de860ea',
-        name: 'name',
-        email: 'name@email.com',
-        surname : 'a',
-        password: 'a'
-    };
+test("Create duplicate user", async () => {
+  const _doc = {
+    _id: "507f191e810c19729de860ea",
+    name: "name",
+    email: "name@email.com",
+    surname: "a",
+    password: "a",
+  };
 
-    mockingoose.User.toReturn(_doc, 'findOne');
+  mockingoose.User.toReturn(_doc, "findOne");
 
-    const createUserPromise = async () => {
-        await createUser('name@email.com',
-            'name',
-            'a',
-            'a')
-    };
-    await expect(createUserPromise).rejects.toThrow();
-
+  const createUserPromise = async () => {
+    await createUser("name@email.com", "name", "a", "a");
+  };
+  await expect(createUserPromise).rejects.toThrow();
 });
 
-test('Create correct user', async () => {
-    mockingoose.User.toReturn(undefined, 'findOne');
+test("Create correct user", async () => {
+  mockingoose.User.toReturn(undefined, "findOne");
 
-    const createUserPromise = async () => {
-        await createUser('name@email.com',
-            'name',
-            'a',
-            'a')
-    };
-    await createUserPromise();
-
+  const createUserPromise = async () => {
+    await createUser("name@email.com", "name", "a", "a");
+  };
+  await createUserPromise();
 });
