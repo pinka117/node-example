1439016cdfe097d21161b087831ac6c793df6222
linda paolieri <me@lindapaolieri.com>
2020-12-21 17:35:49 +0100

    add test to user service

---
+++ src/routes/user/userService.test.js
@@ -0,0 +1,42 @@
+import Mockingoose from 'mockingoose';
+import { createUser } from "./userService.js";
+
+const mockingoose=Mockingoose.default;
+
+test('Create duplicate user', async () => {
+    const _doc = {
+        _id: '507f191e810c19729de860ea',
+        name: 'name',
+        email: 'name@email.com',
+        surname : 'a',
+        password: 'a'
+    };
+
+    mockingoose.User.toReturn(_doc, 'findOne');
+
+    const createUserPromise = async () => {
+        await createUser('name@email.com',
+            'name',
+            'a',
+            'a').then((state) => {
+
+        });
+    };
+    await expect(createUserPromise).rejects.toThrow();
+
+});
+
+test('Create correct user', async () => {
+    mockingoose.User.toReturn(undefined, 'findOne');
+
+    const createUserPromise = async () => {
+        await createUser('name@email.com',
+            'name',
+            'a',
+            'a').then((state) => {
+
+        });
+    };
+    await createUserPromise();
+
+});
