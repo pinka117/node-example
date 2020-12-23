324c6b71785c6dde901e7e30bf6d8b2f746e8ac0
linda paolieri <me@lindapaolieri.com>
2020-12-23 14:55:05 +0100

    refactor middleware for jscon schema validator

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import swaggerUi from 'swagger-ui-express';
 
 import userRouter from "./routes/user/userRouter.js";
 
+import jsonSchemaValidationErrorMiddleware from "./middleware/jsonSchemaValidatorErrorMiddleware.js";
+
 import { connectToDB } from "./utils/db.js";
 
 import swaggerDoc from './dist/swagger.json'
@@ -17,36 +19,7 @@ app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDoc));
 
 app.use("/api/user", userRouter);
-app.use(function(err, req, res, next) {
 
-    var responseData;
+app.use(jsonSchemaValidationErrorMiddleware);
 
-    if (err.name === 'JsonSchemaValidation') {
-        // Log the error however you please
-        console.log(err.message);
-        // logs "express-jsonschema: Invalid data found"
-
-        // Set a bad request http response status or whatever you want
-        res.status(400);
-
-        // Format the response body however you want
-        responseData = {
-           statusText: 'Bad Request',
-           jsonSchemaValidation: true,
-           validations: err.validations  // All of your validation information
-        };
-
-        // Take into account the content type if your app serves various content types
-        if (req.xhr || req.get('Content-Type') === 'application/json') {
-            res.json(responseData);
-        } else {
-            // If this is an html request then you should probably have
-            // some type of Bad Request html template to respond with
-            res.render('badrequestTemplate', responseData);
-        }
-    } else {
-        // pass error to next error middleware handler
-        next(err);
-    }
-});
 export default app;
