492387d9f7cf4dae0775c660292b4a10b8eaf721
Mattia Campagna <mattia.campagna@noku.io>
2021-01-01 12:19:22 +0100

    Env

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import app from "./app.js";
+import { config } from "./configuration/env.js";
 import { connectToDB } from "./utils/db.js";
 
 connectToDB();
 
 // Get port from environment and store in Express.
-const port = process.env.PORT || "3000";
+const port = config.port;
 
 // Get port from environment and store in Express.
 app.listen(port, () => console.log(`App listening on port ${port}`));
