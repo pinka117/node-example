11fcdfe353187b5c6ba5b0385f1fb29f279a559c
Mattia Campagna <mattia.campagna@noku.io>
2021-01-01 12:17:54 +0100

    New env

--- src/configuration/env.js
+++ src/configuration/env.js
@@ -1,5 +1,10 @@
-export const redisConfiguration = {
-  host: process.env.REDIS_URL || "localhost",
-  port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-};
+export const config = {
+  redis: {
+    host: process.env.REDIS_URL || "localhost",
+    port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+    secret: process.env.SESSION_SECRET || 'this is a secret'
+  },
+  isLocalhost: process.env.IS_LOCALHOST || true,
+  port: process.env.PORT || "3000"
+}
